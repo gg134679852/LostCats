d6263d2511126eccc659d3ddced037060accec9d
gg134679852 <metal852794613@gmail.com>
2021-06-29 16:01:13 +0800

    update-routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
