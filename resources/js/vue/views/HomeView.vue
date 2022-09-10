a7553cdfd94eda4013381f07790954f8b0898245
ken <metal852794613@gmail.com>
2022-09-10 10:02:31 +0800

    add-userPage-NavBar

--- resources/js/vue/views/HomeView.vue
+++ resources/js/vue/views/HomeView.vue
@@ -1,8 +1,13 @@
 <template>
+  <NavBarVue />
  <h1>home</h1>
 </template>
 <script>
 // @ is an alias to /src
+import NavBarVue from '../components/NavBar.vue'
 export default {
+  components: {
+    NavBarVue
+  }
 }
 </script>
