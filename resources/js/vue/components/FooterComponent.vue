bc80faa76e6c3384929cb567b3f2149b0ac37ab1
ken <metal852794613@gmail.com>
2022-09-11 13:01:23 +0800

    add-FooterComponent

---
+++ resources/js/vue/components/FooterComponent.vue
@@ -0,0 +1,8 @@
+<template>
+ <footer>
+  <h2>僅供練習用，無商業用途</h2>
+</footer>
+</template>
+<style lang="scss">
+@import'../scss/FooterComponent.scss'
+</style>
--- resources/js/vue/views/App.vue
+++ resources/js/vue/views/App.vue
@@ -1,9 +1,14 @@
 <template>
   <router-view/>
+  <FooterComponentVue />
 </template>
 <script>
 import { Toast } from '../utils/helpers'
+import FooterComponentVue from '../components/FooterComponent.vue'
 export default {
+  components: {
+    FooterComponentVue
+  },
   provide () {
     return {
       Toast
@@ -14,4 +19,5 @@ export default {
 <style lang="scss">
 @import '../scss/all.scss';
 @import '../../../css/app.scss';
+@import '../scss/FooterComponent.scss'
 </style>
