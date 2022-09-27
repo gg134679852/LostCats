34180627ec75ec89efb02768c0248cc7cc1fd89d
ken <metal852794613@gmail.com>
2022-09-27 12:14:23 +0800

    add-dotenv

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import * as dotenv from 'dotenv'
 import App from './vue/views/App.vue'
 import router from './vue/router'
 import store from './vue/store'
@@ -11,14 +12,14 @@ import 'animate.css'
 import { axiosHelper } from './vue/utils/helpers'
 
 require('./bootstrap')
-
+dotenv.config()
 const app = createApp(App)
 app.use(VueAxios, { $axiosHelper: axiosHelper })
 app.use(store)
 app.use(router)
 app.use(VueGoogleMaps, {
   load: {
-    key: env(),
+    key: process.env.MIX_API_KEY
   }
 })
 app.component('loading-icon', Loading)
