31b9510b82c514b981350bd000d8f945c5428532
ken <metal852794613@gmail.com>
2022-10-12 14:56:39 +0800

    change-admin-CatList-button-color

--- resources/js/vue/components/admin/CatList.vue
+++ resources/js/vue/components/admin/CatList.vue
@@ -12,7 +12,7 @@
       </ul>
     </div>
     <div class="button-warp">
-      <button type="button" class="btn btn-primary" @click.prevent="switcher('none','createNewCatData')">新增資料</button>
+      <button type="button" class="btn btn-secondary mx-3" @click.prevent="switcher('none','createNewCatData')">新增資料</button>
       <button type="button" class="btn btn-secondary" @click.prevent="filterSwitcher"><i
           class="fas fa-filter"></i>過濾器</button>
     </div>
