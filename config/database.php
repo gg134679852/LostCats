7bfecd48a2b9dcf800524db5e7db04327f4b9263
ken <metal852794613@gmail.com>
2022-10-15 10:21:56 +0800

    add-heroku-dataBase

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+$db = parse_url(env('CLEARDB_DATABASE_URL'));
+
+
 return [
 
     /*
@@ -46,14 +49,14 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db['host'],
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => ltrim($db['path'],'/'),
+            'username' => $db['user'],
+            'password' => $db['pass'],
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
