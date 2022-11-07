f683ec4559cb2efc8386500b7e7997fd78763769
ken <metal852794613@gmail.com>
2022-11-07 12:02:31 +0800

    edit-getCatData

--- app/Console/Commands/getCatData.php
+++ app/Console/Commands/getCatData.php
@@ -43,12 +43,16 @@ class getCatData extends Command
         echo "程序開始" . PHP_EOL;
         echo "從資料庫拿出既有的貓咪資料" . PHP_EOL;
         $oldAnimalData = AnimalData::all()->toArray();
+        $oldShelterData = ShelterList::all()->toArray();
         echo "獲取寵物領養資料..." . PHP_EOL;
         $response = Http::get('https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL')->json();
         echo "過濾不是貓的寵物領養資料..." . PHP_EOL;
-        $catData = array_filter($response, function ($item) {
-            return ($item['animal_kind'] === '貓');
-        });
+        $catData = [];
+        foreach ($response as $item) {
+            if ($item['animal_kind'] === '貓') {
+                array_push($catData, $item);
+            }
+        };
         function addShelterData($shelterData)
         {
             echo "寫入{$shelterData['shelter_name']}的資料" . PHP_EOL;
@@ -78,12 +82,12 @@ class getCatData extends Command
         function addNewAnimaData($catData)
         {
             echo "新建id名為{$catData['animal_id']}的新貓咪資料" . PHP_EOL;
-            $shelter_id = ShelterList::select('id')->where('shelter_name',$catData['shelter_name'])->get()->toArray();
+            $shelter_id = ShelterList::select('id')->where('shelter_name', $catData['shelter_name'])->get()->toArray();
             AnimalData::create([
                 "animal_id" => $catData['animal_id'],
-                "animal_subid" =>$catData['animal_subid'],
+                "animal_subid" => $catData['animal_subid'],
                 "animal_sex" => $catData['animal_sex'],
-                "animal_bodytype" =>$catData['animal_bodytype'],
+                "animal_bodytype" => $catData['animal_bodytype'],
                 "animal_color" => $catData['animal_color'],
                 "animal_age" => $catData['animal_age'],
                 "animal_sterilization" => $catData['animal_sterilization'],
@@ -91,25 +95,38 @@ class getCatData extends Command
                 "animal_foundplace" => $catData['animal_foundplace'],
                 "animal_remark" => $catData['animal_remark'],
                 "album_file" => $catData['album_file'],
-                "shelter_id" => $shelter_id[0]['id']
+                "shelter_id" => $shelter_id[0]['id'],
             ]);
         }
-
+        function updateAlbumFile($id, $albumFile)
+        {
+            echo "更新id名為{$id}的照片資料" . PHP_EOL;
+            AnimalData::where('animal_id', $id)->update(['album_file' => $albumFile]);
+        }
         if (count($oldAnimalData) !== 0) {
             echo "開始比對新舊資料..." . PHP_EOL;
             foreach ($oldAnimalData as $oldData) {
                 $id = $oldData['animal_id'];
-                $inArray = in_array($id, array_column($catData,'animal_id'));
-                if ($inArray === false) {
+                $arrayIndex = array_search($id, array_column($catData, 'animal_id'));
+                if ($arrayIndex === false) {
                     deleteOldAnimaData($id);
                 }
+                if ($arrayIndex !== false && $oldData['album_file'] === null) {
+                    $albumFile = $catData[$arrayIndex]['album_file'];
+                    updateAlbumFile($id, $albumFile);
+                }
             }
-
         }
-        echo "將收容所資料寫入資料庫..." . PHP_EOL;
+
+        echo "將新資料寫入資料庫..." . PHP_EOL;
+        $isExist = [];
         foreach ($catData as $index => $cat) {
-            $oldShelterData = ShelterList::where('shelter_name', $cat['shelter_name'])->get()->toArray();
-            if (count($oldShelterData) === 0) {
+            $id = $cat['animal_id'];
+            $shelterName = $cat['shelter_name'];
+            $catDataArrayIndex = array_search($id, array_column($oldAnimalData, 'animal_id'));
+            $shelterDataArrayIndex = array_search($shelterName, array_column($oldShelterData, 'shelter_name'));
+            $isExistArrayIndex = in_array($shelterName, $isExist,true);
+            if ($shelterDataArrayIndex === false && $isExistArrayIndex === false) {
                 $newShelterData =
                     [
                     "shelter_name" => $cat['shelter_name'],
@@ -117,18 +134,14 @@ class getCatData extends Command
                     "shelter_city" => mb_substr($cat['shelter_address'], 0, 3, 'UTF-8'),
                     "shelter_tel" => $cat['shelter_tel'],
                 ];
+                array_push($isExist,$cat['shelter_name']);
                 addShelterData($newShelterData);
             }
-
-        }
-        echo "將貓咪資料寫入資料庫..." . PHP_EOL;
-        foreach ($catData as $index => $cat) {
-            $oldCatData = AnimalData::where('animal_id', $cat['animal_id'])->get()->toArray();
-            if (count($oldCatData) === 0) {
+            if ($catDataArrayIndex === false) {
                 $newCatData =
                     [
                     "animal_id" => $cat['animal_id'],
-                    "animal_subid" =>$cat['animal_subid'],
+                    "animal_subid" => $cat['animal_subid'],
                     "animal_sex" => $cat['animal_sex'] === 'F' ? '女生' : '男生',
                     "animal_bodytype" => $cat['animal_bodytype'] === 'SMALL' ? '小型' : ($cat['animal_bodytype'] === 'MEDIUM' ? '中型' : '大型'),
                     "animal_color" => $cat['animal_colour'],
@@ -137,13 +150,12 @@ class getCatData extends Command
                     "animal_bacterin" => $cat['animal_bacterin'] === 'F' ? '未打狂犬病疫苗' : '已打狂犬病疫苗',
                     "animal_foundplace" => $cat['animal_foundplace'],
                     "animal_remark" => $cat['animal_remark'],
-                    "album_file" => $cat['album_file']  === '' ? NULL:$cat['album_file'],
-                    "shelter_name" => $cat['shelter_name']
+                    "album_file" => $cat['album_file'] === '' ? null : $cat['album_file'],
+                    "shelter_name" => $cat['shelter_name'],
                 ];
                 addNewAnimaData($newCatData);
             }
         }
         echo "程序結束" . PHP_EOL;
     }
-
 }
